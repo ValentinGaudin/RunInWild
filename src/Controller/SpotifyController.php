31c1c30d6bc9d2dae377d43f8c7cd44c4c426e37
jgautrais <j.gautrais@gmail.com>
2021-11-25 17:26:44 +0100

    adjust

--- src/Controller/SpotifyController.php
+++ src/Controller/SpotifyController.php
@@ -25,21 +25,13 @@ class SpotifyController extends AbstractController
             "auth_bearer" => $token
         ]);
 
-            $players = $client->request("GET", "https://api.spotify.com/v1/me/player", [
-                'headers' => [
-                    "Accept" => "application/json",
-                    "Content-Type" => "application/json"],
-                "auth_bearer" => $token]);
-
         if ($response->getStatusCode() == 200) {
             $results = $response->toArray();
-            $player = $players->getContent();
 
             $playlists = $results['playlists']['items'];
 
-            $id = $playlists ['1']['id'];
-            return $this->twig->render('Spotify/index.html.twig', ['results' => $results, 'id' => $id, 'player' => $player]);
-
+            $id = $playlists['1']['id'];
+            return $this->twig->render('Spotify/index.html.twig', ['results' => $results, 'id' => $id]);
         }
 
         return $response->getStatusCode();
